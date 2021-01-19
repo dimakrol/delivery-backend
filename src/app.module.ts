df670e4fe40590cea9d90a6ec4af4eccdad7d3c9
Dima Krol <dmitriy.krol.v@gmail.com>
2021-01-19 21:55:52 +0200

    Installed typeorm and connected to database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'delivery',
+      password: 'delivery',
+      database: 'delivery',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       autoSchemaFile: true,
