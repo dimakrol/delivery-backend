95eb593eaf6cf589a423fbed63065cb87b962f7e
Dima Krol <dmitriy.krol.v@gmail.com>
2021-01-19 19:00:12 +0200

    Added first dto, created first mutation with dto arguments.

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,11 +1,26 @@
-import {Args, Query, Resolver} from '@nestjs/graphql';
+import {Args, Mutation, Query, Resolver} from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
+import {CreateRestaurantDto} from "./dtos/create-restaurant.dto";
 
 @Resolver((of) => Restaurant)
 export class RestaurantsResolver {
   @Query((returns) => [Restaurant])
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    console.log(veganOnly);
     return [];
   }
+
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
+  }
+  // @Mutation((returns) => Boolean)
+  // createRestaurant(
+  //   @Args('name') name: string,
+  //   @Args('isVegan') isVegan: boolean,
+  //   @Args('address') address: string,
+  //   @Args('ownerName') ownerName: string,
+  // ): boolean {
+  //   return true;
+  // }
 }
