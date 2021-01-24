43245d79ed1baa3756c6ed54e0e737ea1bfe6e14
Dima Krol <dmitriy.krol.v@gmail.com>
2021-01-24 20:42:43 +0200

    Continue edit user functionality.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -70,7 +70,7 @@ export class UsersService {
     return this.users.findOne({ id });
   }
 
-  async editProfile(userId: number, { email, password }: EditProfileInput) {
-    return this.users.update({ id: userId }, { email, password });
+  async editProfile(userId: number, editProfileInput: EditProfileInput) {
+    return this.users.update({ id: userId }, { ...editProfileInput });
   }
 }
