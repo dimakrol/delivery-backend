1135e50355b35d8baaaaac2602f0d26132f3c7e1
Dima Krol <dmitriy.krol.v@gmail.com>
2021-01-24 20:54:35 +0200

    Finish update user functionality.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -70,7 +70,17 @@ export class UsersService {
     return this.users.findOne({ id });
   }
 
-  async editProfile(userId: number, editProfileInput: EditProfileInput) {
-    return this.users.update({ id: userId }, { ...editProfileInput });
+  async editProfile(
+    userId: number,
+    { email, password }: EditProfileInput,
+  ): Promise<User> {
+    const user = await this.users.findOne(userId);
+    if (email) {
+      user.email = email;
+    }
+    if (password) {
+      user.password = password;
+    }
+    return this.users.save(user);
   }
 }
