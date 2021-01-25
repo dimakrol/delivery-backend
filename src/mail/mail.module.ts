47e2e5b6953b066c1ae093e71e5522382e69b0eb
Dima Krol <dmitriy.krol.v@gmail.com>
2021-01-25 23:47:58 +0200

    Start of mail module.

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,20 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { CONFIG_OPTIONS } from '../common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Module({})
+export class MailModule {
+  static forRoot(options: MailModuleOptions): DynamicModule {
+    return {
+      module: MailModule,
+      exports: [],
+      providers: [
+        MailModule,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+      ],
+    };
+  }
+}
