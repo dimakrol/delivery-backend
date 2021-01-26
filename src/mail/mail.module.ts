102a418354deafe619f371218077b74cdaf9102e
Dima Krol <dmitriy.krol.v@gmail.com>
2021-01-26 09:22:49 +0200

    Implemented test send mail functionality.

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,15 +1,16 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { CONFIG_OPTIONS } from '../common/common.constants';
 import { MailModuleOptions } from './mail.interfaces';
+import { MailService } from './mail.service';
 
 @Module({})
 export class MailModule {
   static forRoot(options: MailModuleOptions): DynamicModule {
     return {
       module: MailModule,
-      exports: [],
+      exports: [MailService],
       providers: [
-        MailModule,
+        MailService,
         {
           provide: CONFIG_OPTIONS,
           useValue: options,
