e591b7f988311324cf81b73a16634f5a34fe2cd4
Dima Krol <dmitriy.krol.v@gmail.com>
2021-01-28 15:49:05 +0200

    Implemented findById test.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -83,11 +83,11 @@ export class UsersService {
 
   async findById(id: number): Promise<UserProfileOutput> {
     try {
-      const user = await this.users.findOne({ id });
+      const user = await this.users.findOneOrFail({ id });
       if (user) {
         return {
           ok: true,
-          user: user,
+          user,
         };
       }
     } catch (error) {
