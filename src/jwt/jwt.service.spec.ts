4ce3d14029754f488431bf7287aa9525caf33ac7
Dima Krol <dmitriy.krol.v@gmail.com>
2021-01-29 15:14:19 +0200

    Sing method test for jwt service.

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,6 +1,13 @@
 import { JwtService } from './jwt.service';
 import { Test } from '@nestjs/testing';
 import { CONFIG_OPTIONS } from '../common/common.constants';
+import * as jwt from 'jsonwebtoken';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'secret_token'),
+  };
+});
 
 const TEST_KEY = 'testKey';
 
@@ -12,7 +19,7 @@ describe('JwtService', () => {
         JwtService,
         {
           provide: CONFIG_OPTIONS,
-          useValue: { privateKey: 'some_test_pk' },
+          useValue: { privateKey: TEST_KEY },
         },
       ],
     }).compile();
@@ -24,6 +31,16 @@ describe('JwtService', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('sign');
-  it.todo('verify');
+  describe('sign', () => {
+    it('should return a signed token', async () => {
+      const ID = 1;
+      const token = service.sign(ID);
+      expect(typeof token).toBe('string');
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: ID }, TEST_KEY);
+    });
+  });
+  describe('verify', () => {
+    it('should return decoded token', () => {});
+  });
 });
