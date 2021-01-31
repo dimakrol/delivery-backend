1d5ff14f1b508805f2b86cb67d0ed62a17edd5de
Dima Krol <dmitriy.krol.v@gmail.com>
2021-01-31 21:38:10 +0200

    Implemented e2e test for createAccount.

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { getConnection } from 'typeorm';
 
+const GRAPHQL_ENDPOINT = '/graphql';
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -21,7 +23,32 @@ describe('UserModule (e2e)', () => {
     await app.close();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'joker@gmail.com';
+    it('should create an account', async () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              createAccount(input: {
+                email: "${EMAIL}",
+                password: "123132123",
+                role: Client})
+              {
+                ok,
+                error
+              }
+            }
+            `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+  });
   it.todo('userProfile');
   it.todo('login');
   it.todo('me');
