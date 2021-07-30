33e3a3de69ca4c8b9429d86589eb3eeb66a908a3
dimakrol <dmitriy.krol.v@gmail.com>
2021-07-30 04:57:12 +0300

    Created payment entity.

---
+++ src/payments/entities/payment.entity.ts
@@ -0,0 +1,28 @@
+import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, ManyToOne, RelationId } from 'typeorm';
+import { CoreEntity } from '../../common/entities/core.entity';
+import { User } from '../../users/entities/user.entity';
+import {Restaurant} from "../../restaurants/entities/restaurant.entity";
+
+@InputType('PaymentInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class Payment extends CoreEntity {
+  @Field(() => Int)
+  @Column()
+  transactionId: number;
+
+  @Field(() => User)
+  @ManyToOne(() => User, (user) => user.payments)
+  user: User;
+
+  @RelationId((payment: Payment) => payment.user)
+  userId: number;
+
+  @Field(() => Restaurant)
+  @ManyToOne(() => Restaurant)
+  restaurant: Restaurant;
+
+  @RelationId((payment: Payment) => payment.restaurant)
+  restaurantId: number;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -11,6 +11,7 @@ import { InternalServerErrorException } from '@nestjs/common';
 import { IsBoolean, IsEmail, IsEnum, IsString } from 'class-validator';
 import { Restaurant } from '../../restaurants/entities/restaurant.entity';
 import { Order } from '../../orders/entities/order.entity';
+import {Payment} from "../../payments/entities/payment.entity";
 
 export enum UserRole {
   Client = 'Client',
@@ -54,6 +55,10 @@ export class User extends CoreEntity {
   @Field(() => [Order])
   @OneToMany(() => Order, (order) => order.customer)
   orders: Order[];
+
+  @Field(() => [Payment])
+  @OneToMany(() => Payment, (payment) => payment.user)
+  payments: Payment[];
 
   @Field(() => [Order])
   @OneToMany(() => Order, (order) => order.driver)
